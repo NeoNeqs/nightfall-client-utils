629ebf4dc4cfc693f59c18ba7341f4d332d8c983
Michael March <theneqstech@gmail.com>
2021-03-22 11:43:36 +0100

    Minor fixes

--- Scripts/Services/NetworkedClient.cs
+++ Scripts/Services/NetworkedClient.cs
@@ -1,29 +1,26 @@
 using Godot;
 
 using SharedUtils.Common;
-using SharedUtils.Exceptions;
 using SharedUtils.Services;
-using SharedUtils.Loaders;
+using SharedUtils.Networking;
 
 namespace ClientsUtils.Services
 {
-    public abstract class NetworkedClient : NetworkedPeer
+    public abstract class NetworkedClient<T> : NetworkedPeer<T> where T : Node
     {
-        protected NetworkedClient() : base()
-        {
-        }
+        protected NetworkedClient() : base() { }
+
 
         public override void _EnterTree()
         {
             base._EnterTree();
-            SetupDTLS();
+            _ = SetupDTLS();
         }
 
-        public override void _Ready()
-        {
-            ConnectSignals();
-        }
-        
+        public override void _Ready() => base._Ready();
+
+        public override void _Process(float delta) => base._Process(delta);
+
         public ErrorCode CreateClient(string ipAddress, int port)
         {
             var error = _peer.CreateClient(ipAddress, port);
@@ -31,32 +28,20 @@ namespace ClientsUtils.Services
             return (ErrorCode)((int)error);
         }
 
-        protected void SetupDTLS()
-        {
-            string path = "user://DTLS/";
-            base.SetupDTLS(path);
-
-            ErrorCode error;
-            _peer.SetDtlsCertificate(X509CertificateLoader.Load(path, GetCertificateName(), out error));
-            if (error != ErrorCode.Ok)
-            {
-                throw new X509CertificateNotFoundException($"Failed to load x509 certificate from '{path.PlusFile(GetCertificateName())}'");
-            }
-        }
-
         protected override void ConnectSignals()
         {
-            CustomMultiplayer.Connect("connection_failed", this, nameof(ConnetionFailed));
-            CustomMultiplayer.Connect("connected_to_server", this, nameof(ConnectionSuccessful));
-            CustomMultiplayer.Connect("server_disconnected", this, nameof(ServerDisconnected));
+            CustomMultiplayer.Connect("connection_failed", this, nameof(_ConnectionFailed));
+            CustomMultiplayer.Connect("connected_to_server", this, nameof(_ConnectionSuccessful));
+            CustomMultiplayer.Connect("server_disconnected", this, nameof(_ServerDisconnected));
         }
-        
+
         protected void Send(params object[] args)
         {
-            RpcId(1, "OnPeerPacket", args);
+            RpcId(1, "PacketReceived", args);
         }
-        protected abstract void ConnetionFailed();
-        protected abstract void ConnectionSuccessful();
-        protected abstract void ServerDisconnected();
+
+        protected abstract void _ConnectionFailed();
+        protected abstract void _ConnectionSuccessful();
+        protected abstract void _ServerDisconnected();
     }
 }

77f9e79bc8794345d0d16a7a7ed3b2e4ea7f3842
NeqsTech <theneqstech@gmail.com>
2021-09-27 23:53:27 +0200

    Branch moved to 3.x. From now on main is developed on Godot 4.0.

--- Scripts/Exceptions/CantConnectToServerException.cs
+++
@@ -1,9 +0,0 @@
-using SharedUtils.Exceptions;
-
-namespace ClientsUtils.Exceptions
-{
-    public class CantConnectToServerException : NightFallException
-    {
-        public CantConnectToServerException(string ip, int port) : base($"Could not create connection to server {ip}:{port}.") { }
-    }
-}
